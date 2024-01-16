54118cf71e4c9c75c9258d0491d19152257f3a0f
Viny Selopal <vinyselopal@Vinys-MacBook-Air.local>
2024-01-16 15:04:24 +0530

    feat: initiate the app and create Doctypes

---
+++ inventory_management/inventory_management/doctype/reports/test_reports.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Viny Selopal and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestReports(FrappeTestCase):
+	pass
---
+++ inventory_management/inventory_management/doctype/stock_entry/stock_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Viny Selopal and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Stock Entry", {
+// 	refresh(frm) {
+
+// 	},
+// });
