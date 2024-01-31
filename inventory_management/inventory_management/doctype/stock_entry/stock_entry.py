e73b9ea8f85d24540150acb70aef3f7388668de0
Viny Selopal <viny0698@gmail.com>
2024-01-31 17:13:57 +0530

    format stock entry controller and test files

--- inventory_management/inventory_management/doctype/stock_entry/stock_entry.py
+++ inventory_management/inventory_management/doctype/stock_entry/stock_entry.py
@@ -9,57 +9,65 @@ from pypika.terms import Case
 from pypika import Field
 from pypika import functions as fn
 
+
 class StockEntry(Document):
+    def create_sle(self, warehouse, qty, stock_ledger_item):
+        new_sle_doc = frappe.new_doc("Stock Ledger Entry")
+        new_sle_doc.item = stock_ledger_item
+        new_sle_doc.warehouse = warehouse
+        new_sle_doc.qty_change = qty
 
-	def create_sle(self, warehouse,qty, stock_ledger_item):
-		new_sle_doc = frappe.new_doc('Stock Ledger Entry')
-		new_sle_doc.item = stock_ledger_item
-		new_sle_doc.warehouse = warehouse
-		new_sle_doc.qty_change = qty
+        stock_ledger_entries = frappe.qb.from_("Stock Ledger Entry")
+        item = stock_ledger_item.item
 
-		stock_ledger_entries = frappe.qb.from_("Stock Ledger Entry")
-		item = stock_ledger_item.item
+        item_field = frappe.qb.Field("item")
+        qty_change_field = frappe.qb.Field("qty_change")
+        valuation_rate_field = frappe.qb.Field("valuation_rate")
 
-		item_field = frappe.qb.Field("item")
-		qty_change_field = frappe.qb.Field("qty_change")
-		valuation_rate_field = frappe.qb.Field("valuation_rate")
+        item_stock_ledger_entries = stock_ledger_entries.select("*").where(
+            item_field == item
+        )
 
-		item_stock_ledger_entries = stock_ledger_entries.select("*").where(item_field == item)
+        ttl_valuation_rate = Sum(valuation_rate_field * qty_change_field).as_(
+            "val_rate_sum"
+        )
+        response = item_stock_ledger_entries.select(ttl_valuation_rate).run(
+            as_dict=True
+        )
+        stock_ledger_old_valuation_rates_sum = response[0].val_rate_sum or 0
 
-		ttl_valuation_rate = Sum(valuation_rate_field * qty_change_field).as_("val_rate_sum")
-		response = item_stock_ledger_entries.select(ttl_valuation_rate).run(as_dict=True)
-		stock_ledger_old_valuation_rates_sum = response[0].val_rate_sum or 0
+        ttl_qty = Sum(item_stock_ledger_entries.qty_change).as_("qtysum")
+        response = item_stock_ledger_entries.select(ttl_qty).run(as_dict=True)
+        print("response", response)
+        stock_ledger_ttl_qtys = response[0].qtysum or 0
 
-		ttl_qty = Sum(item_stock_ledger_entries.qty_change).as_("qtysum")
-		response = item_stock_ledger_entries.select(ttl_qty).run(as_dict=True)
-		print("response", response)
-		stock_ledger_ttl_qtys = response[0].qtysum or 0
+        valuation_rate = (
+            stock_ledger_old_valuation_rates_sum
+            + (stock_ledger_item.rate * stock_ledger_item.qty)
+        ) / (stock_ledger_ttl_qtys + stock_ledger_item.qty)
+        new_sle_doc.valuation_rate = valuation_rate
+        new_sle_doc.insert()
 
-		valuation_rate = (stock_ledger_old_valuation_rates_sum + (stock_ledger_item.rate * stock_ledger_item.qty)) / (stock_ledger_ttl_qtys + stock_ledger_item.qty)
-		new_sle_doc.valuation_rate = valuation_rate
-		new_sle_doc.insert()
+    def on_submit(self):
+        for item in self.items:
+            if self.type == "Receive":
+                self.create_sle(item.target_warehouse, item.qty, item)
 
-	def on_submit(self):
-		for item in self.items:
-			if self.type == "Receive":
-				self.create_sle(item.target_warehouse, item.qty, item)
+            elif self.type == "Consume":
+                self.create_sle(item.source_warehouse, -item.qty, item)
 
-			elif self.type == "Consume":
-				self.create_sle(item.source_warehouse, -item.qty, item)
+            else:
+                self.create_sle(item.target_warehouse, item.qty, item)
+                self.create_sle(item.source_warehouse, -item.qty, item)
 
-			else:
-				self.create_sle(item.target_warehouse, item.qty, item)
-				self.create_sle(item.source_warehouse, -item.qty, item)
+    def on_cancel(self):
+        for item in self.items:
+            if self.type == "Receive":
+                self.create_sle(item.target_warehouse, -item.qty, item)
 
-	def on_cancel(self):
-		for item in self.items:
-			if self.type == "Receive":
-				self.create_sle(item.target_warehouse, - item.qty, item)
+            elif self.type == "Consume":
+                self.create_sle(item.source_warehouse, item.qty, item)
 
-			elif self.type == "Consume":
-				self.create_sle(item.source_warehouse, item.qty, item)
-
-			else:
-				self.create_sle(item.target_warehouse, - item.qty, item)
-				self.create_sle(item.source_warehouse, item.qty, item)
-	
+            else:
+                self.create_sle(item.target_warehouse, -item.qty, item)
+                self.create_sle(item.source_warehouse, item.qty, item)
--- inventory_management/inventory_management/doctype/stock_entry/test_stock_entry.py
+++ inventory_management/inventory_management/doctype/stock_entry/test_stock_entry.py
@@ -6,14 +6,24 @@ import frappe
 from frappe.utils import getdate, today, datetime
 import datetime
 
-class TestStockEntry(FrappeTestCase):
 
+class TestStockEntry(FrappeTestCase):
     def setUp(self):
         self.create_test_parent_child_warehouses()
         self.create_test_items()
-    
+
     def tearDown(self):
         frappe.db.rollback()
+
+    def testStockLedgerEntryFromStockEntry(self):
+        self.create_test_stock_entry_for_transfer()
+        stock_ledger_entry = frappe.get_doc(
+            doctype="Stock Ledger Entry",
+            posting_date=frappe.utils.getdate(),
+            name="nord_ledger_entry",
+            qty_change=1,
+        )
+        self.assertTrue(stock_ledger_entry)
 
     def create_test_parent_child_warehouses(self):
         parent_warehouse = frappe.new_doc("Warehouse")
@@ -31,49 +41,44 @@ class TestStockEntry(FrappeTestCase):
         item1.opening_warehouse = "child_warehouse"
         item1.opening_qty = 30
 
-        item1.insert(ignore_if_duplicate = True)
+        item1.insert(ignore_if_duplicate=True)
         item2 = frappe.new_doc("Item")
         item2.item_code = "boat"
         item2.opening_warehouse = "parent_warehouse"
         item2.opening_qty = 20
 
         item2.insert(ignore_if_duplicate=True)
-        
 
     def create_test_stock_entry_for_transfer(self):
-        doc = frappe.new_doc("Stock Entry")    
+        doc = frappe.new_doc("Stock Entry")
         doc.date = today()
         doc.type = "Transfer"
 
         doc.append(
             "items",
-                {
-                    "item": datetime.datetime.strftime(datetime.date.today(), '%y-%m-%d') + "-" + "nord",
-                    "qty": 1,
-                    "rate": 3000,
-                    "name": "nord_ledger_entry",
-                    "source_warehouse": "child_warehouse",
-                    "target_Warehouse": "parent_warehouse",
-                }
+            {
+                "item": datetime.datetime.strftime(datetime.date.today(), "%y-%m-%d")
+                + "-"
+                + "nord",
+                "qty": 1,
+                "rate": 3000,
+                "name": "nord_ledger_entry",
+                "source_warehouse": "child_warehouse",
+                "target_Warehouse": "parent_warehouse",
+            },
         )
         doc.append(
             "items",
-                {
-                    "item": datetime.datetime.strftime(datetime.date.today(), '%y-%m-%d') + "-" + "boat",
-                    "qty": 1,
-                    "rate": 5000,
-                    "name": "boat_ledger_entry",
-                    "source_warehouse": "parent_warehouse",
-                    "target_Warehouse": "child_warehouse",
-                }
+            {
+                "item": datetime.datetime.strftime(datetime.date.today(), "%y-%m-%d")
+                + "-"
+                + "boat",
+                "qty": 1,
+                "rate": 5000,
+                "name": "boat_ledger_entry",
+                "source_warehouse": "parent_warehouse",
+                "target_Warehouse": "child_warehouse",
+            },
         )
         doc.insert(ignore_if_duplicate=True)
         doc.submit()
-        
-    def testStockLedgerEntryFromStockEntry(self):
-        self.create_test_stock_entry_for_transfer()
-        stock_ledger_entry = frappe.get_doc(doctype="Stock Ledger Entry", posting_date=frappe.utils.getdate(), name="nord_ledger_entry", qty_change=1)
-        self.assertTrue(stock_ledger_entry)
-
-
-    
