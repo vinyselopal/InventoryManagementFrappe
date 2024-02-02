47cd48c144a73932826f9a6e66d11f738cab71d9
Viny Selopal <viny0698@gmail.com>
2024-02-02 11:48:05 +0530

    create validation methods, move util functions out of main class

--- inventory_management/inventory_management/doctype/stock_entry/stock_entry.py
+++ inventory_management/inventory_management/doctype/stock_entry/stock_entry.py
@@ -3,71 +3,69 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.query_builder import DocType
 from frappe.query_builder.functions import Sum
-from pypika.terms import Case
-from pypika import Field
-from pypika import functions as fn
 
 
 class StockEntry(Document):
-    def create_sle(self, warehouse, qty, stock_ledger_item):
-        new_sle_doc = frappe.new_doc("Stock Ledger Entry")
-        new_sle_doc.item = stock_ledger_item
-        new_sle_doc.warehouse = warehouse
-        new_sle_doc.qty_change = qty
+    def validate(self):
+        self.validate_balance_qty()
+        self.validate_mandatory_warehouses()
 
-        stock_ledger_entries = frappe.qb.from_("Stock Ledger Entry")
-        item = stock_ledger_item.item
+    def validate_balance_qty(self):
+        pass
 
-        item_field = frappe.qb.Field("item")
-        qty_change_field = frappe.qb.Field("qty_change")
-        valuation_rate_field = frappe.qb.Field("valuation_rate")
-
-        item_stock_ledger_entries = stock_ledger_entries.select("*").where(
-            item_field == item
-        )
-
-        ttl_valuation_rate = Sum(valuation_rate_field * qty_change_field).as_(
-            "val_rate_sum"
-        )
-        response = item_stock_ledger_entries.select(ttl_valuation_rate).run(
-            as_dict=True
-        )
-        stock_ledger_old_valuation_rates_sum = response[0].val_rate_sum or 0
-
-        ttl_qty = Sum(item_stock_ledger_entries.qty_change).as_("qtysum")
-        response = item_stock_ledger_entries.select(ttl_qty).run(as_dict=True)
-        print("response", response)
-        stock_ledger_ttl_qtys = response[0].qtysum or 0
-
-        valuation_rate = (
-            stock_ledger_old_valuation_rates_sum
-            + (stock_ledger_item.rate * stock_ledger_item.qty)
-        ) / (stock_ledger_ttl_qtys + stock_ledger_item.qty)
-        new_sle_doc.valuation_rate = valuation_rate
-        new_sle_doc.insert()
+    def validate_mandatory_warehouses(self):
+        pass
 
     def on_submit(self):
         for item in self.items:
             if self.type == "Receive":
-                self.create_sle(item.target_warehouse, item.qty, item)
+                create_sle(item.target_warehouse, item.qty, item)
 
             elif self.type == "Consume":
-                self.create_sle(item.source_warehouse, -item.qty, item)
+                create_sle(item.source_warehouse, -item.qty, item)
 
             else:
-                self.create_sle(item.target_warehouse, item.qty, item)
-                self.create_sle(item.source_warehouse, -item.qty, item)
+                create_sle(item.target_warehouse, item.qty, item)
+                create_sle(item.source_warehouse, -item.qty, item)
 
     def on_cancel(self):
         for item in self.items:
             if self.type == "Receive":
-                self.create_sle(item.target_warehouse, -item.qty, item)
+                create_sle(item.target_warehouse, -item.qty, item)
 
             elif self.type == "Consume":
-                self.create_sle(item.source_warehouse, item.qty, item)
+                create_sle(item.source_warehouse, item.qty, item)
 
             else:
-                self.create_sle(item.target_warehouse, -item.qty, item)
-                self.create_sle(item.source_warehouse, item.qty, item)
+                create_sle(item.target_warehouse, -item.qty, item)
+                create_sle(item.source_warehouse, item.qty, item)
+
+
+def create_sle(warehouse: str, qty: float, item: dict) -> None:
+    sle = frappe.new_doc("Stock Ledger Entry")
+    sle.item = item.item
+    sle.warehouse = warehouse
+    sle.qty_change = qty
+    sle.valuation_rate = get_valuation_rate(item)
+    sle.insert()
+
+
+def get_valuation_rate(item: dict) -> float:
+    StockLedgerEntry = frappe.qb.DocType("Stock Ledger Entry")
+
+    result = (
+        frappe.qb.from_(StockLedgerEntry)
+        .select(
+            Sum(StockLedgerEntry.valuation_rate * StockLedgerEntry.qty_change).as_("valuation_rate_sum"),
+            Sum(StockLedgerEntry.qty_change).as_("qty_change"),
+        )
+        .where(
+            (StockLedgerEntry.item == item)
+            & (StockLedgerEntry.docstatus == 1)
+        )
+    ).run(as_dict=True)
+
+    return (
+       (result[0].valuation_rate_sum + (item.rate * item.qty)) / (result[0].qty_change + item.qty)
+    ) if result else 0
