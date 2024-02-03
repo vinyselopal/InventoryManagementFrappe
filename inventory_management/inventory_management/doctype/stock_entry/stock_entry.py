1eba959c7b1bf6ef6cf28e3523524c5aff9552c9
Viny Selopal <viny0698@gmail.com>
2024-02-03 17:09:30 +0530

    add validations and refactor tests

--- inventory_management/inventory_management/doctype/stock_entry/stock_entry.py
+++ inventory_management/inventory_management/doctype/stock_entry/stock_entry.py
@@ -5,41 +5,51 @@ import frappe
 from frappe.model.document import Document
 from frappe.query_builder.functions import Sum
 
-
 class StockEntry(Document):
     def validate(self):
         self.validate_balance_qty()
         self.validate_mandatory_warehouses()
 
     def validate_balance_qty(self):
-        pass
+        for item in self.items:
+            if self.type == "Consume" or self.type == "Transfer":
+                item_warehouse_balance = check_warehouse_balance(item.source_warehouse, item.name, item.qty)
+                return True if item_warehouse_balance > 0 else False
 
     def validate_mandatory_warehouses(self):
-        pass
+        for item in self.items:
+            if (self.type == "Consume" and item.source_warehouse == None):
+                return False
+            if (self.type == "Receive" and item.target_warehouse == None):
+                return False
+            if (self.type == "Transfer" and (item.source_warehouse == None or item.target_warehouse == None)):
+                return False
+            else: 
+                return True
 
     def on_submit(self):
         for item in self.items:
             if self.type == "Receive":
-                create_sle(item.target_warehouse, item.qty, item)
+                create_sle(item.target_warehouse, item.qty, item, None)
 
             elif self.type == "Consume":
-                create_sle(item.source_warehouse, -item.qty, item)
+                create_sle(item.source_warehouse, -item.qty, item, None)
 
             else:
-                create_sle(item.target_warehouse, item.qty, item)
-                create_sle(item.source_warehouse, -item.qty, item)
+                create_sle(item.target_warehouse, item.qty, item, None)
+                create_sle(item.source_warehouse, -item.qty, item, None)
 
     def on_cancel(self):
         for item in self.items:
             if self.type == "Receive":
-                create_sle(item.target_warehouse, -item.qty, item)
+                create_sle(item.target_warehouse, -item.qty, item, None)
 
             elif self.type == "Consume":
-                create_sle(item.source_warehouse, item.qty, item)
+                create_sle(item.source_warehouse, item.qty, item, None)
 
             else:
-                create_sle(item.target_warehouse, -item.qty, item)
-                create_sle(item.source_warehouse, item.qty, item)
+                create_sle(item.target_warehouse, -item.qty, item, None)
+                create_sle(item.source_warehouse, item.qty, item, None)
 
 
 def create_sle(warehouse: str, qty: float, item: dict, valuation_rate: int) -> None:
@@ -69,3 +79,20 @@ def get_valuation_rate(item: dict) -> float:
     return (
        (result[0].valuation_rate_sum + (item.rate * item.qty)) / (result[0].qty_change + item.qty)
     ) if result else 0
+
+def check_warehouse_balance(warehouse, item, qty):
+    StockLedgerEntry = frappe.qb.DocType("Stock Ledger Entry")
+
+    result = (
+        frappe.qb.from_(StockLedgerEntry)
+        .select(
+            "qty_change",
+            Sum(StockLedgerEntry.qty_change).as_("qty_balance")
+        )
+        .where(
+            (StockLedgerEntry.item == item)
+            & (StockLedgerEntry.warehouse == warehouse)
+        )
+    ).run(as_dict=True)
+
+    return result[0].qty_balance or 0
--- inventory_management/inventory_management/doctype/stock_entry/test_stock_entry.py
+++ inventory_management/inventory_management/doctype/stock_entry/test_stock_entry.py
@@ -3,17 +3,20 @@
 
 from frappe.tests.utils import FrappeTestCase
 import frappe
-from frappe.utils import today, datetime
-import datetime
-
+from frappe.utils import today
 
 class TestStockEntry(FrappeTestCase):
     def test_stock_ledger_entry(self):
-        parent_warehouse, child_warehouse = self.create_test_parent_child_warehouses()
-        item1 = create_test_item("nord", "child_warehouse", 30)
-        item2 = create_test_item("boat", "parent_warehouse", 20)
+        parent_warehouse, child_warehouse = create_test_parent_child_warehouses()
 
-        self.create_test_stock_entry_for_transfer()
+        item1, stock_entry_item1 = create_test_item(
+            "nord", "child_warehouse", 30, 1, 3000, child_warehouse, parent_warehouse, "nord_ledger_entry"
+        )
+        item2, stock_entry_item2 = create_test_item(
+            "boat", "parent_warehouse", 20, 1, 5000, parent_warehouse, child_warehouse, "boat_ledger_entry"
+        )
+
+        create_test_stock_entry_for_transfer([stock_entry_item1, stock_entry_item2])
         stock_ledger_entry = frappe.get_doc(
             doctype="Stock Ledger Entry",
             posting_date=frappe.utils.getdate(),
@@ -37,46 +40,42 @@ def create_test_parent_child_warehouses() -> tuple[str, str]:
     return parent_warehouse.name, child_warehouse.name
 
 
-def create_test_item(item, opening_warehouse, opening_qty) -> dict:
+def create_test_item(
+    item,
+    opening_warehouse,
+    opening_qty,
+    sle_qty,
+    sle_rate,
+    source_warehouse,
+    target_warehouse,
+    sle_name
+) -> dict:
     item_doc = frappe.new_doc("Item")
     item_doc.item_code = item
     item_doc.opening_warehouse = opening_warehouse
     item_doc.opening_qty = opening_qty
     item_doc.insert(ignore_if_duplicate=True)
 
-    return item_doc
+    slei_doc = frappe._dict({
+        "item": item_doc,
+        "qty": sle_qty,
+        "rate": sle_rate,
+        "source_warehouse": source_warehouse,
+        "target_warehouse": target_warehouse,
+        "name": sle_name
+    })
+
+    return item_doc, slei_doc
 
 
-def create_test_stock_entry_for_transfer():
+def create_test_stock_entry_for_transfer(stock_entry_items):
     doc = frappe.new_doc("Stock Entry")
     doc.date = today()
     doc.type = "Transfer"
 
-    doc.append(
-        "items",
-        {
-            "item": datetime.datetime.strftime(datetime.date.today(), "%y-%m-%d")
-            + "-"
-            + "nord",
-            "qty": 1,
-            "rate": 3000,
-            "name": "nord_ledger_entry",
-            "source_warehouse": "child_warehouse",
-            "target_Warehouse": "parent_warehouse",
-        },
-    )
-    doc.append(
-        "items",
-        {
-            "item": datetime.datetime.strftime(datetime.date.today(), "%y-%m-%d")
-            + "-"
-            + "boat",
-            "qty": 1,
-            "rate": 5000,
-            "name": "boat_ledger_entry",
-            "source_warehouse": "parent_warehouse",
-            "target_Warehouse": "child_warehouse",
-        },
-    )
+    for item in stock_entry_items:
+        print(item)
+        doc.append("items", item)
+
     doc.insert(ignore_if_duplicate=True)
     doc.submit()
