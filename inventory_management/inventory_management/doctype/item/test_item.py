2d6d9f59a2cd6d6d9025057f17c1042f8635409b
Viny Selopal <viny0698@gmail.com>
2024-02-07 16:33:14 +0530

    test stock ledger creation on item insertion

--- inventory_management/inventory_management/doctype/item/test_item.py
+++ inventory_management/inventory_management/doctype/item/test_item.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2024, Viny Selopal and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
+from ..stock_entry.test_stock_entry import create_test_parent_child_warehouses, create_test_item
 
 
 class TestItem(FrappeTestCase):
-	pass
+	def test_sle_valuation_rate_on_item_creation(self):
+		parent_warehouse, child_warehouse = create_test_parent_child_warehouses()
+
+		item1 = create_test_item(
+			"nord", "child_warehouse", 30, 4000
+		)
+		item_valuation_rate = frappe.db.get_value("Stock Ledger Entry", {"item": item1.name}, "valuation_rate")
+		self.assertEqual(item_valuation_rate, item1.opening_valuation_rate)
